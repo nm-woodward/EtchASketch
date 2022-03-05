eb539efb6e84f5cfd91cb24651879dd7526b0f31
nm-woodward <nowoodward15@gmail.com>
2022-03-05 15:42:17 -0800

    Converted sketchboard creation to function

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-const ncols = 15;
-const nrows = 15;
+const ncols = 64;
+const nrows = 64;
 
 const sketchDestination = document.querySelector('.middle-panel');
+const body = document.querySelector('body');
 
-const sketchBoard = document.createElement("div");
-sketchBoard.classList.toggle('sketch-board');
+function createSketchBoard(nrows,ncols) {
 
-for (let i=1; i <= nrows; i++) {
-    const newRow = document.createElement("div");
-    newRow.classList.toggle('sketch-row');
-
-    for (let i=1; i <= ncols; i++){
-        const newCol = document.createElement("div");
-        newCol.textContent = '';
-        newCol.classList.toggle('sketch-cell');
-        newRow.appendChild(newCol);
+    if (sketchBoard != null) {
+        sketchDestination.removeChild(sketchBoard)
     }
-    sketchBoard.appendChild(newRow);
+    
+    const sketchBoard = document.createElement("div");
+    sketchBoard.classList.toggle('sketch-board');
+    
+    for (let i=1; i <= nrows; i++) {
+        const newRow = document.createElement("div");
+        newRow.classList.toggle('sketch-row');
+    
+        for (let i=1; i <= ncols; i++){
+            const newCol = document.createElement("div");
+            newCol.textContent = '';
+            newCol.classList.toggle('sketch-cell');
+            newRow.appendChild(newCol);
+        }
+        sketchBoard.appendChild(newRow);
+    }
+    
+    sketchDestination.appendChild(sketchBoard);
+
 }
 
-sketchDestination.appendChild(sketchBoard);
 
+createSketchBoard(20,20);
 
 
 // Reset cell height when screen is res
@@ -34,3 +45,47 @@ cells.forEach( (cell) => {
 
 
 console.log(sketchDestination);
+
+// Set up mouse down/up toggle as prereq for drawing and erasing
+
+let toggle = false;
+let eraser = false;
+
+body.addEventListener('mousedown', () => toggle = true);
+body.addEventListener('mouseup', () => toggle = false);
+
+// Add draw function to each cell
+
+cells.forEach( (cell) => {
+    cell.addEventListener('mouseover', function changeColor() {
+        if (toggle && eraser) {
+            cell.classList.remove('activated');
+        }
+        else if (toggle) {
+            cell.classList.add('activated');
+        }
+        
+    })
+})
+
+// Add eraser event listener for eraser button
+
+const eraserButton = document.querySelector(".eraser");
+
+eraserButton.addEventListener('click', () => {
+    eraser ? eraser = false : eraser = true
+    });
+
+
+// Create clear function
+const clearButton = document.querySelector(".clear");
+
+function clearCells(cells) {
+    cells.forEach( (cell), () => cell.classList.remove('activated'))
+}
+
+clearButton.addEventListener('click', () => {
+    cells.forEach( (cell) => {
+        cell.classList.remove('activated')
+    })
+});
